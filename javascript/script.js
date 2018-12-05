ca0dd1545d1f9fd8660e9533f08321f3305eb525
Nasséra <45563624+DashingNass@users.noreply.github.com>
2018-12-05 12:03:27 +0100

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -4,10 +4,3 @@ var sortForm = document.getElementById("sort-form");
 sortButton.addEventListener("click", function() {
     sortForm.classList.toggle("clicked");
 });
-
-var filter = document.getElementById("filter-button");
-var filterForm = document.getElementById("filter-form");
-
-filter.addEventListener("click", function() {
-    filterForm.classList.toggle("clicked");
-});
